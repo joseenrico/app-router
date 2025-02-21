290c1538e42813aefa4959e6112eecb171458c9f
Iko Localoka <joseenriconapitupulu@gmail.com>
2025-02-21 13:39:57 +0700

    setup next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,30 @@
 "use client";
 
-import { useState } from "react"; // Import useState
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { useState } from "react";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navbar";
 import { usePathname } from "next/navigation";
+import { SessionProvider } from "next-auth/react";
 
-// Configure the Geist Sans font
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-// Configure the Geist Mono font
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const disableNavbar = ["/auth/login", "/register"]; 
+const inter = Inter({ subsets: ["latin"] });
+const disableNavbar = ["/auth/login", "/register"];
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [state, setState] = useState(0);
   const pathname = usePathname();
 
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {!disableNavbar.includes(pathname) && <Navbar />}
-        
-        {children}
+      <body className={inter.className}>
+        <SessionProvider>
+          {!disableNavbar.includes(pathname) && <Navbar />}
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
